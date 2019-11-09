8974fef1eca9b628b0e62ab09610dbf55a60f373
Rick Campbell <agentofmisfortune@gmail.com>
2019-11-09 18:49:20 -0500

    Updates

---
+++ precache-manifest.38da49874f80a5882f944ebd289c17fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec02923c3e6549a1b15b6e57c6accc2a",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "9626bb84d207abeeb086",
+    "url": "/starwars/static/css/main.f4b70abe.chunk.css"
+  },
+  {
+    "revision": "193def658c6c3aef1480",
+    "url": "/starwars/static/js/2.0b84d54e.chunk.js"
+  },
+  {
+    "revision": "9626bb84d207abeeb086",
+    "url": "/starwars/static/js/main.84f1b68e.chunk.js"
+  },
+  {
+    "revision": "94c37c5547c1369de5a6",
+    "url": "/starwars/static/js/runtime-main.d999b6dc.js"
+  },
+  {
+    "revision": "e630d69eaf6c15f915e06f39de6c4280",
+    "url": "/starwars/static/media/space.e630d69e.jpg"
+  }
+]);
